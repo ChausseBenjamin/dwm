b58c3c0c560d9f31fb3d14aa02b87746d8744da2
Benjamin Chausse <benjamin@chausse.xyz>
2024-01-31 21:37:13 -0500

    Fix Compile flag removal error

--- vanitygaps.c
+++ vanitygaps.c
@@ -17,6 +17,7 @@ static void tile(Monitor *m);
 static void getgaps(Monitor *m, int *oh, int *ov, int *ih, int *iv, unsigned int *nc);
 static void getfacts(Monitor *m, int msize, int ssize, float *mf, float *sf, int *mr, int *sr);
 static void setgaps(int oh, int ov, int ih, int iv);
+static int enablegaps = 1;
 
 void
 setgaps(int oh, int ov, int ih, int iv)
